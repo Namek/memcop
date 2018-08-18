e7144c1ad464cae2de4e48beb4a9076fae16bc68
Namek <kamil@namekdev.net>
2018-08-18 02:25:02 +0200

    assembly code compilation + most of transputer job

---
+++ core/src/net/namekdev/memcop/domain/Assembly.java
@@ -0,0 +1,292 @@
+package net.namekdev.memcop.domain;
+
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.namekdev.memcop.domain.Assembly.OpArgType.*;
+
+public abstract class Assembly {
+
+    public static final Register[] registers = new Register[] {
+        reg("$0", true, false),
+        reg("$a", true, true),
+        reg("$b", true, true),
+        reg("$c", true, true),
+        reg("$d", true, true),
+        reg("$di", true, true)
+    };
+    public static TreeMap<String, Register> registerByName = new TreeMap<String, Register>();
+
+    public static final OpArg $Value = new OpArg(new OpArgType[] {Register, Const});
+    public static final OpArg $Label = new OpArg(new OpArgType[] {Label, Const});
+    public static final OpArg $OutRegister = new OpArg(new OpArgType[] {Register}, true);
+    public static final OpArg $Register = new OpArg(new OpArgType[] {Register}, false);
+
+    public static final OpDefinition[] opdefs = new OpDefinition[] {
+        op("add", $Value, $Value, $OutRegister).describe("a + b -> c"),
+        op("sub", $Value, $Value, $OutRegister).describe("a - b -> c"),
+        op("mul", $Value, $Value, $OutRegister).describe("a * b -> c"),
+        op("div", $Value, $Value, $OutRegister).describe("a / b -> c"),
+        op("and", $Value, $Value, $OutRegister).describe("a & b -> c"),
+        op("orl", $Value, $Value, $OutRegister).describe("a || b -> c"),
+        op("xor", $Value, $Value, $OutRegister).describe("a ^ b -> c"),
+        op("push", $Value).describe("push value on stack"),
+        op("pop", $OutRegister).describe("remove top value from the stack and put it into specified register"),
+        op("top", $OutRegister).describe("get top stack value stack without removing it"),
+        op("cmp", $Value, $Value, $OutRegister),
+        op("ldb", $OutRegister).describe("load one source byte to specified register"),
+        op("stb", $Register, $Value).describe("store byte from given register to given index inside destination memory"),
+        op("transfer", $Value).describe("load byte and put it to specified index inside destination memory"),
+        op("jump", $Label).describe("unconditional jump"),
+        op("je", $Value, $Label).describe("jump if equal to"),
+        op("jne", $Value, $Label).describe("jump if not equal to"),
+        op("jg", $Value, $Label).describe("jump if greater than"),
+        op("jl", $Value, $Label).describe("jump if less than"),
+        op("jge", $Value, $Label).describe("jump if greater or equal to"),
+        op("jle", $Value, $Label).describe("jump if lesser or equal to"),
+    };
+    public static TreeMap<String, OpDefinition> opdefByName = new TreeMap<String, OpDefinition>();
+
+    static {
+        for (Register reg : registers)
+            registerByName.put(reg.name, reg);
+
+        for (OpDefinition oc : opdefs)
+            opdefByName.put(oc.instructionName, oc);
+    }
+
+    public static class Register {
+        public final int id;
+        public final String name;
+        public boolean readable;
+        public boolean writable;
+
+        public Register(int id, String name, boolean readable, boolean writable) {
+            this.id = id;
+            this.name = name;
+            this.readable = readable;
+            this.writable = writable;
+        }
+    }
+
+    public static class OpDefinition {
+        public final int id;
+        public final String instructionName;
+        public final OpArg[] args;
+        public String description;
+
+        public OpDefinition(int id, String name, OpArg[] args) {
+            this.id = id;
+            this.instructionName = name;
+            this.args = args;
+        }
+
+        public OpDefinition describe(String description)
+        {
+            this.description = description;
+            return this;
+        }
+    }
+
+    public static class OpArg {
+        public final OpArgType[] allowedTypes;
+        public final boolean isReturnType;
+
+        public OpArg(OpArgType[] opArgTypes) {
+            this.allowedTypes = opArgTypes;
+            this.isReturnType = false;
+        }
+
+        public OpArg(OpArgType[] opArgTypes, boolean isOut) {
+            this.allowedTypes = opArgTypes;
+            this.isReturnType = isOut;
+        }
+    }
+
+    public enum OpArgType {
+        Register,
+        Const,
+        Label;
+    }
+
+
+    public static class Instruction {
+        public OpDefinition opdef;
+        public InstrArg[] args;
+    }
+
+    public static class InstrArg {
+        public OpArgType argType;
+        public String token;
+
+        /**
+         * depending on argType it is: constant value, register id or relative offset for label
+         */
+        public int constVal = -1;
+    }
+
+    public static class Label {
+        public String name;
+        public int line = -1;
+        public int instructionIndex = -1;
+    }
+
+    private static int _regsCreated = 0;
+    private static Register reg(String name, boolean readable, boolean writable) {
+        return new Register(_regsCreated++, name, readable, writable);
+    }
+
+    private static int _opdefsCreated = 0;
+    private static OpDefinition op(String str, OpArg... args) {
+        return new OpDefinition(_opdefsCreated++, str, args);
+    }
+
+
+    private static Pattern labelPointRegex = Pattern.compile("([_a-zA-Z]+[_a-zA-Z0-9]+):");
+    private static Pattern labelNameRegex = Pattern.compile("([_a-zA-Z]+[_a-zA-Z0-9]+)");
+    private static Pattern constRegex = Pattern.compile("-?\\d+");
+    private static Pattern registerRegex = Pattern.compile("\\$[a-z][a-z0-9]?");
+
+    public static List<Instruction> compile(String code) throws AssemblyCompilationError {
+        final TreeMap<String, Label> labels = new TreeMap<String, Label>();
+
+        // 1. scan for labels
+        StringTokenizer t = new StringTokenizer(code, "\r\n");
+        int line = 0;
+        while (t.hasMoreTokens()) {
+            String token = t.nextToken().trim();
+            line += 1;
+
+            Matcher match = labelPointRegex.matcher(token);
+            if (!match.matches())
+                continue;
+
+            String labelName = match.group(1);
+            Label label = new Label();
+            label.name = labelName;
+            label.line = line;
+
+            if (labels.containsKey(labelName)) {
+                Label existingLabel = labels.get(labelName);
+                throw new AssemblyCompilationError("Label `" + labelName + "` is already defined in line " + existingLabel.line, line);
+            }
+
+            labels.put(labelName, label);
+        }
+
+        // 2. compile assembly
+        final List<Instruction> instructions = new ArrayList<Instruction>(line);
+        line = 0;
+        t = new StringTokenizer(code, "\r\n");
+        while (t.hasMoreTokens())
+        {
+            String lineStr = t.nextToken().trim();
+            line += 1;
+
+            // omit empty line
+            if (lineStr.length() == 0)
+                continue;
+
+            // update label's instruction index, then continue
+            Matcher labelMatch = labelPointRegex.matcher(lineStr);
+            if (labelMatch.matches()) {
+                String labelName = labelMatch.group(1);
+                Label label = labels.get(labelName);
+
+                if (label == null)
+                    throw new AssemblyCompilationError("Label `" + labelName + "` was never defined", line);
+
+                label.instructionIndex = instructions.size();
+
+                continue;
+            }
+
+            // it's supposed to be a instruction line, analyze it
+            StringTokenizer instrTokenizer = new StringTokenizer(lineStr, " \t\n\r\f");
+            String token = instrTokenizer.nextToken();
+
+            OpDefinition opdef = opdefByName.get(token);
+
+            if (opdef == null)
+                throw new AssemblyCompilationError("Unknown instruction `" + token + "`", line);
+
+            Instruction instr = new Instruction();
+            instr.opdef = opdef;
+            instr.args = new InstrArg[opdef.args.length];
+
+            int argIndex = 0;
+            for (OpArg opArg : opdef.args) {
+                token = instrTokenizer.nextToken();
+
+                OpArgType instrArgType = registerRegex.matcher(token).matches() ? Register :
+                        constRegex.matcher(token).matches() ? Const :
+                        labelNameRegex.matcher(token).matches() ? Label : null;
+
+                if (instrArgType == null) {
+                    throw new AssemblyCompilationError("unrecognized token `" + token + "`", line);
+                }
+
+                if (Arrays.binarySearch(opArg.allowedTypes, instrArgType) >= 0) {
+                    InstrArg instrArg = new InstrArg();
+                    instrArg.token = token;
+                    instrArg.argType = instrArgType;
+
+                    if (instrArgType == Const) {
+                        instrArg.constVal = Integer.parseInt(token);
+                    }
+                    else if (instrArgType == Label) {
+                        Label label = labels.get(token);
+
+                        if (label == null)
+                            throw new AssemblyCompilationError("Label `" + token + "` is not defined", line);
+
+                        // relative offset for label is calculated in the 3rd step
+                    }
+                    else if (instrArgType == Register) {
+                        Register reg = registerByName.get(token);
+
+                        if (reg == null)
+                            throw new AssemblyCompilationError("Register `" + token + "` does not exist", line);
+
+                        instrArg.constVal = reg.id;
+                    }
+
+                    instr.args[argIndex++] = instrArg;
+                }
+                else {
+                    String nth = argIndex == 0 ? "1st" : argIndex == 1 ? "2nd" : argIndex == 2 ? "3rd" : (argIndex+1) + "st";
+                    String msg = "instruction " + opdef.instructionName + ": argument of type " + instrArgType + " is not allowed for " + nth + " argument";
+                    throw new AssemblyCompilationError(msg, line);
+                }
+
+                line += 1;
+            }
+        }
+
+        // 3. replace labels with constants: calculate relative offsets
+        int instrIndex = 0;
+        for (Instruction instr : instructions) {
+            for (InstrArg arg : instr.args) {
+                if (arg.argType == Label) {
+                    Label label = labels.get(arg.token);
+                    int relativeOffset = label.instructionIndex - instrIndex;
+                    arg.constVal = relativeOffset;
+                    arg.argType = Const;
+                }
+            }
+
+            instrIndex += 1;
+        }
+
+        return instructions;
+    }
+
+
+    public static class AssemblyCompilationError extends Exception {
+        public AssemblyCompilationError(String message, int line) {
+            super("Error in line  " + line + " : " + message);
+        }
+    }
+}
---
+++ core/src/net/namekdev/memcop/domain/Transputer.java
@@ -0,0 +1,290 @@
+package net.namekdev.memcop.domain;
+
+import java.util.List;
+import java.util.Stack;
+import java.util.TreeMap;
+import net.namekdev.memcop.domain.Assembly.Instruction;
+import net.namekdev.memcop.domain.Assembly.InstrArg;
+import net.namekdev.memcop.domain.Assembly.OpArgType;
+import net.namekdev.memcop.domain.Assembly.OpDefinition;
+
+public class Transputer {
+    private class RegisterState {
+        public Assembly.Register info;
+        public int value;
+    }
+
+    private interface InstrExecutor {
+        /**
+         *
+         * @param instr
+         * @return new cursor or -1 if instruction don't want to jump
+         */
+        int execute(Instruction instr);
+    }
+
+
+    /**
+     * Defines a position before given instruction is executed.
+     */
+    public int instrCursor = 0;
+    public List<Instruction> instructions;
+    public Stack<Integer> stack = new Stack<Integer>();
+
+    public final int stackMaxSize;
+    public final RegisterState[] registerStates;
+    public final InstrExecutor[] instructionExecutors;
+
+    public final TreeMap<String, InstrExecutor> instructionExecutorByName = new TreeMap<String, InstrExecutor>() {{
+        put("add", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                getReg(instr.args[2]).value = value1 + value2;
+
+                return -1;
+            }
+        });
+        put("sub", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                getReg(instr.args[2]).value = value1 - value2;
+
+                return -1;
+            }
+        });
+        put("mul", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                getReg(instr.args[2]).value = value1 * value2;
+
+                return -1;
+            }
+        });
+        put("div", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                getReg(instr.args[2]).value = value1 / value2;
+
+                return -1;
+            }
+        });
+        put("and", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                getReg(instr.args[2]).value = value1 & value2;
+
+                return -1;
+            }
+        });
+        put("orl", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                getReg(instr.args[2]).value = value1 | value2;
+
+                return -1;
+            }
+        });
+        put("xor", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                getReg(instr.args[2]).value = value1 ^ value2;
+
+                return -1;
+            }
+        });
+        put("push", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value = getValue(instr.args[0]);
+                stack.push(value);
+
+                if (stack.size() >= stackMaxSize)
+                    stack.setSize(stackMaxSize);
+
+                return -1;
+            }
+        });
+        put("pop", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                RegisterState reg = getReg(instr.args[0]);
+
+                int value = 0;
+                if (!stack.isEmpty())
+                    value = stack.pop();
+
+                reg.value = value;
+
+                return -1;
+            }
+        });
+        put("top", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                RegisterState reg = getReg(instr.args[0]);
+
+                int value = 0;
+                if (!stack.isEmpty())
+                    value = stack.peek();
+
+                reg.value = value;
+
+                return -1;
+            }
+        });
+        put("cmp", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int value1 = getValue(instr.args[0]);
+                int value2 = getValue(instr.args[1]);
+                RegisterState reg = getReg(instr.args[2]);
+
+                int cmp = value2 - value1;
+                reg.value = cmp > 0 ? 1 : cmp < 0 ? -1 : 0;
+
+                return -1;
+            }
+        });
+        put("ldb", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                RegisterState reg = getReg(instr.args[0]);
+                reg.value = loadSourceByte();
+
+                return -1;
+            }
+        });
+        put("stb", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int srcByte = getValue(instr.args[0]);
+                int destIndex = getValue(instr.args[1]);
+                storeDestinationByte(destIndex, srcByte);
+
+                return -1;
+            }
+        });
+        put("transfer", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int destIndex = getValue(instr.args[0]);
+                int srcByte = loadSourceByte();
+                storeDestinationByte(destIndex, srcByte);
+
+                return -1;
+            }
+        });
+        put("jump", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                int relativeOffset = getValue(instr.args[0]);
+                return instrCursor + relativeOffset;
+            }
+        });
+        put("je", new InstrExecutor() {
+            @Override
+            public int execute(Instruction instr) {
+                // TODO this needs a proper definition. What are we equaling this to?
+                int relativeOffset = getValue(instr.args[1]);
+                return instrCursor + relativeOffset;
+            }
+        });
+    }};
+
+
+    public Transputer(int stackMaxSize) {
+        this.stackMaxSize = stackMaxSize;
+
+        // initialize registers
+        int n = Assembly.registers.length;
+        registerStates = new RegisterState[n];
+
+        for (int i = 0; i < n; ++i) {
+            Assembly.Register regInfo = Assembly.registers[i];
+            RegisterState reg = new RegisterState();
+            reg.info = regInfo;
+            reg.value = 0;
+            registerStates[i] = reg;
+        }
+
+        // initialize instructions
+        n = Assembly.opdefs.length;
+        instructionExecutors = new InstrExecutor[n];
+
+        for (int i = 0; i < n; ++i) {
+            OpDefinition opdef = Assembly.opdefs[i];
+            instructionExecutors[i++] = instructionExecutorByName.get(opdef.instructionName);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            if (instructionExecutors[i] == null) {
+                OpDefinition opdef = Assembly.opdefs[i];
+                throw new Error("instruction `" + opdef.instructionName + "` has not been implemented");
+            }
+        }
+    }
+
+
+    /**
+     * Execute a single instruction.
+     */
+    public void forward() {
+        Instruction instr = instructions.get(instrCursor);
+        InstrExecutor instrExec = instructionExecutors[instr.opdef.id];
+        int newInstrCursor = instrExec.execute(instr);
+
+        if (newInstrCursor >= 0)
+            instrCursor = newInstrCursor;
+        else
+            instrCursor += 1;
+    }
+
+    public void reset() {
+        instrCursor = 0;
+        for (RegisterState reg : registerStates) {
+            reg.value = 0;
+        }
+    }
+
+    private int getValue(InstrArg instrArg) {
+        int value;
+
+        if (instrArg.argType == OpArgType.Register) {
+            int regId = instrArg.constVal;
+            RegisterState reg = registerStates[regId];
+            value = reg.value;
+        }
+        else {
+            value = instrArg.constVal;
+        }
+
+        return value;
+    }
+
+    private RegisterState getReg(InstrArg instrArg) {
+        if (instrArg.argType != OpArgType.Register)
+            throw new Error("expected register address");
+
+        return registerStates[instrArg.constVal];
+    }
+
+    private void storeDestinationByte(int destIndex, int srcByte) {
+    }
+
+    private int loadSourceByte() {
+    }
+}
