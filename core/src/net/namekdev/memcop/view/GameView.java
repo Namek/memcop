28ed071601816a37c00658fa4ebf4cb7876200ce
Namek <kamil@namekdev.net>
2018-08-22 00:20:43 +0200

    highlight code

--- core/src/net/namekdev/memcop/view/GameView.java
+++ core/src/net/namekdev/memcop/view/GameView.java
@@ -9,22 +9,23 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.github.czyzby.lml.annotation.LmlAction;
 import com.github.czyzby.lml.annotation.LmlActor;
 import com.github.czyzby.lml.parser.impl.AbstractLmlView;
+import com.kotcrab.vis.ui.util.highlight.BaseHighlighter;
+import com.kotcrab.vis.ui.widget.HighlightTextArea;
 import net.namekdev.memcop.MemcopGame;
 import net.namekdev.memcop.domain.Assembly;
 import net.namekdev.memcop.domain.Assembly.AssemblyCompilationError;
 import net.namekdev.memcop.domain.Level;
-import net.namekdev.memcop.domain.Sector;
 import net.namekdev.memcop.domain.Transputer;
+import net.namekdev.memcop.view.widgets.LmlSourceHighlighter;
 
 public class GameView extends AbstractLmlView {
     @LmlActor("random") private Label result;
 
-    //@LmlActor("templateInput") private CodeTextArea templateInput;
+    @LmlActor("codeInput") private HighlightTextArea codeInput;
 
 
     public Level level = Level.create2();
@@ -78,6 +79,7 @@ public class GameView extends AbstractLmlView {
             public boolean keyDown(InputEvent event, int keycode) {
                 if (keycode == Input.Keys.ESCAPE)
                     Gdx.app.exit();
+
                 return super.keyDown(event, keycode);
             }
         });
@@ -92,6 +94,11 @@ public class GameView extends AbstractLmlView {
     @Override
     public String getViewId() {
         return "first";
+    }
+
+    @LmlAction("codeHighlighter")
+    public BaseHighlighter getCodeSourceHighlighter() {
+        return new LmlSourceHighlighter();
     }
 
     @LmlAction("roll")
